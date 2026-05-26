cc775f6c084ddaf20233fef4f186233c7d432b98
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-05-27 00:17:53 +0700

    - Add HelloWorld component - Add react root (main.jsx) - Setup vite for multiple html files - Add hello-world.html file

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+        hello_world: "hello-world.html"
+      }
+    }
+  }
 })
