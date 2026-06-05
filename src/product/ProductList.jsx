65f2df4599215bc07b2befb38a4e423d9e5ce053
fadhliazhar14 <fadhliazhar44@gmail.com>
2026-06-05 23:03:31 +0700

    Learn how to use react - Effect Dependencies

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -3,20 +3,37 @@ import Product from "./Product";
 
 export default function ProductList() {
     const [products, setProducts] = useState([]);
-    const isLoaded = useRef(false);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
-        if (isLoaded.current === false) {
+        if (isLoaded) {
             fetch("/product.json")
                 .then(response => response.json())
                 .then(data => setProducts(data))
-                .then(() => isLoaded.current = true);
         }
-    }, []);
+
+        return () => {
+            console.log("Component unmounted");
+        }
+    }, [isLoaded]);
+
+    useEffect(() => {
+        console.log("Use effect dengan empty dependency");
+    }, [])
+
+    useEffect(() => {
+        console.log("Use effect yang akan dipanggil tiap kali component dirender ulang");
+    })
+
+    function handleClick() {
+        setIsLoaded(true);
+    }
 
     return (
         <>
             <h1>Product List</h1>
+            <button onClick={handleClick}>Load Product</button>
+
             {
                 products.map(product => (
                     <Product key={product.id} product={product} />
